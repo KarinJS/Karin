1b7626ab7b10519f66f44b8c8027296d9b40e606
Lain. <1072411694@qq.com>
2024-04-15 18:55:19 +0800

    fix：del global.Karin

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-import Karin from './lib/index.js'
 import Bot from './lib/config/index.js'
 import logger from './lib/config/log.js'
 
 /** 初始化Bot */
 await Bot.start()
-/** 全局变量Karin */
-global.Karin = Karin
 /** 全局变量logger */
 global.logger = logger
