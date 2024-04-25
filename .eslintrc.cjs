b02f76fefc946b8940866041f21432801fb9734a
Lain. <zhiyu955@gmail.com>
2024-04-25 19:35:50 +0800

    :art: fix: 修复部分语法错误，增加更新工具，新增一个开发依赖

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,13 +11,51 @@ module.exports = {
   globals: {
     logger: true
   },
+  plugins: [
+    'import',
+    'promise'
+  ],
   rules: {
     eqeqeq: ['off'],
     'prefer-const': ['off'],
     'arrow-body-style': 'off',
     camelcase: [0, {
       properties: 'always'
-    }]
+    }],
+    'import/no-unresolved': 2,
+    'import/named': 2,
+    'import/default': 2,
+    'import/namespace': 2,
+    'promise/always-return': 'error',
+    'promise/no-return-wrap': 'error',
+    'promise/param-names': 'error',
+    'promise/catch-or-return': 'error',
+    'promise/no-native': 'off',
+    'promise/no-nesting': 'warn',
+    'promise/no-promise-in-callback': 'warn',
+    'promise/no-callback-in-promise': 'warn',
+    'promise/avoid-new': 'off',
+    'promise/no-new-statics': 'error',
+    'promise/no-return-in-finally': 'warn',
+    'promise/valid-params': 'warn',
+    'no-unused-vars': ['error', { args: 'all' }]
   },
-  ignorePatterns: ['lib/adapter/kritor/protos/compiled.js']
+  settings: {
+    'import/resolver': {
+      alias: [
+        ['#Karin', './lib/index.js']
+      ]
+    },
+    node: {
+      version: '>=18.0.0'
+    }
+  },
+  ignorePatterns: [
+    '/data/',
+    '/logs/',
+    '/resources/',
+    '/temp/',
+    'node_modules/',
+    'lib/adapter/kritor/protos/compiled.js'
+  ]
 }
