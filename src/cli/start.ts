7e9ac29d1668d13f7d233ebd4c16f99e520cced2
时瑾 <shijinn520@gmail.com>
2024-09-13 00:11:43 +0800

    fix: close #162

--- src/cli/start.ts
+++ src/cli/start.ts
@@ -1,6 +1,9 @@
 import { program } from 'commander'
 import { KarinCli, Mode, Runner, Lang } from './index'
 
+/** 兼容node .启动方式 */
+if (process.argv.length === 2) process.argv.push('.')
+
 const karin = new KarinCli()
 program.version(karin.pkg(true).version, '-v, --version', '显示版本号')
 program.command('.').description('启动karin').action(() => karin.start(Mode.Prod, Lang.Js, Runner.Node))
