00ba4045a09ea9147bdcdda24c702c92856e8b53
时瑾 <shijinn520@gmail.com>
2024-09-14 08:06:35 +0800

    fix: 修正读取自定义配置

--- src/utils/config/config.ts
+++ src/utils/config/config.ts
@@ -245,7 +245,7 @@ export const config = new (class Cfg {
     const key = 'change.group'
     /** 取缓存 */
     const res = this.change.get(key)
-    const keys = e?.self_id ? [`Bot.${e.self_id}.${group_id}`, `Bot.${e.self_id}`, group_id] : [group_id]
+    const keys = e?.self_id ? [`Bot:${e.self_id}:${group_id}`, `Bot:${e.self_id}`, group_id] : [group_id]
     if (res) {
       const cfg = { ...res.defCfg.default, ...res.Config.default }
       for (const k of keys) {
