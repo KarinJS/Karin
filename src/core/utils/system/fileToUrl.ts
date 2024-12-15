a2e14be7735760286d9baf954ea77e97c5d349a3
时瑾 <74231782+sj817@users.noreply.github.com>
2024-12-15 18:12:53 +0800

    fix: `fileToUrl` 修改返回值类型

--- src/core/utils/system/fileToUrl.ts
+++ src/core/utils/system/fileToUrl.ts
@@ -4,6 +4,18 @@ import type { Event } from '@/event'
 /** fileToUrl Handler键 */
 export const fileToUrlHandlerKey = 'fileToUrl'
 
+type Args = { e?: Event } & Record<string, any>
+type FileToUrlResult<T> = T extends 'image' ? { url: string, width: number, height: number } : { url: string }
+
+export type FileToUrlHandler = {
+  <T extends 'image' | 'video' | 'record' | 'file'> (
+    type: T,
+    file: string,
+    filename: string,
+    args?: Args
+  ): Promise<FileToUrlResult<T>>
+}
+
 /**
  * 文件转换为url
  * @param file 文件路径
@@ -11,16 +23,7 @@ export const fileToUrlHandlerKey = 'fileToUrl'
  * @param filename 文件名名称: `image.jpg`
  * @param args 自定义参数
  */
-export const fileToUrl = async (
-  file: string,
-  type: 'image' | 'video' | 'record' | 'file',
-  filename: string,
-  args?: { e?: Event } & Record<string, any>
-): Promise<{
-  /** 文件链接 */
-  url: string
-  /** 此参数为图片类型专属 */
-}> => {
+export const fileToUrl: FileToUrlHandler = async (type, file, filename, args) => {
   if (!handler.has(fileToUrlHandlerKey)) throw new Error('[Handler][Error]: 没有配置文件转换为url的处理器')
 
   return handler(fileToUrlHandlerKey, { file, type, filename, args })
