dbebe37c9dcaeee31b49cfd5a962b3f18a6bd9a8
时瑾 <74231782+sj817@users.noreply.github.com>
2024-12-19 00:12:23 +0800

    feat: 优化重启函数，支持环境检查并返回重启状态

--- src/core/utils/system/restart.ts
+++ src/core/utils/system/restart.ts
@@ -6,9 +6,14 @@ import { exec } from '@/utils/system/exec'
  * @param selfId - 机器人的id 传e.self_id
  * @param contact - 事件联系人信息 也就是从哪来的这条消息 传e.contact即可
  * @param messageId - 消息id 传e.message_id
- * @param isFront - 是否为前台重启 默认是
+ * @param isFront - 是否为前台重启 默认是 不支持的环境会强制为pm2重启
  */
-export const restart = async (selfId: string, contact: Contact, messageId: string, isFront = true) => {
+export const restart = async (
+  selfId: string,
+  contact: Contact,
+  messageId: string,
+  isFront = true
+): Promise<{ status: 'success' | 'failed'; data: string | Error }> => {
   const options = {
     selfId,
     contact,
@@ -20,9 +25,9 @@ export const restart = async (selfId: string, contact: Contact, messageId: strin
   const { level } = await import('../../../main/index')
   await level.set(key, options)
 
-  if (isFront) {
-    if (!process.send) return { status: 'failed', data: '前台重启失败，当前环境不支持，仅支持【npx karin start】启动的环境' }
+  if (isFront && process.send) {
     process.send('restart')
+    return { status: 'success', data: '已发送重启信号' }
   }
 
   const { config } = await import('@/utils/config')
