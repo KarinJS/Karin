32c4a9b380b9037995f1ffb46aae43674f20ba92
时瑾 <74231782+sj817@users.noreply.github.com>
2025-01-10 02:04:42 +0800

    chore: remove deprecated main entry files and related scripts

--- src/main/up.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @description 更新全部插件
- */
-export async function up () {
-  const { updateAllPkg, updateAllGitPlugin } = await import('@/utils/system/update')
-  await updateAllPkg()
-  await updateAllGitPlugin()
-  process.exit(0)
-}
