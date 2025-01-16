b8b7c995cbd1fcd957e298f704fbafaa21b1a330
时瑾 <74231782+sj817@users.noreply.github.com>
2025-01-17 05:42:18 +0800

    fix: 热更新`.env`未更新日志等级

--- packages/core/src/utils/config/env.ts
+++ packages/core/src/utils/config/env.ts
@@ -5,16 +5,18 @@ import chokidar from 'chokidar'
 /**
  * 监听.env文件变化并自动重新加载
  */
-export const watchEnv = () => {
+export const watchEnv = async () => {
   const targetPath = path.join(process.cwd(), '.env')
   const watcher = chokidar.watch(targetPath, { persistent: true, ignoreInitial: true })
 
   /** 备份RUNTIME */
   const runtime = process.env.RUNTIME
-  watcher.on('change', () => {
+  watcher.on('change', async () => {
     logger.info('[配置文件变动] .env')
     dotenv.config({ path: targetPath, override: true })
     process.env.RUNTIME = runtime
+    const { updateLevel } = await import('@/utils/config/config')
+    updateLevel(process.env.LOG_LEVEL)
   })
 }
 
