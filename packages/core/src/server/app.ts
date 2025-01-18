833eb2a7bd40225d074f51b21cbfa1261d681665
时瑾 <74231782+sj817@users.noreply.github.com>
2025-01-19 05:53:08 +0800

    feat: webui

--- packages/core/src/server/app.ts
+++ packages/core/src/server/app.ts
@@ -3,23 +3,14 @@ import express from 'express'
 import { createServer } from 'node:http'
 import { router } from './api/router'
 import { listeners } from '@/core/internal'
+import { authMiddleware } from './middleware'
 
 import type { Express } from 'express'
-import { authMiddleware } from './middleware'
 
 /** express 服务 */
 export const app: Express = express()
 /** http 服务 */
 export const server = createServer(app)
-
-/** 中间件鉴权 */
-app.use(authMiddleware)
-
-// TODO: WEB
-app.use('/web', express.static(path.join(process.cwd(), 'web')))
-app.use('/web/*', express.static(path.join(process.cwd(), 'web')))
-
-app.use('/api/v1', router)
 
 /**
  * 监听端口
@@ -32,8 +23,38 @@ export const listen = (port: number, host: string) => {
   })
 
   listeners.once('online', () => {
+    logger.info(`[server] webui 已启动 正在监听: http://${host}:${port}/web`)
     /** logger会记录到日志文件中 */
     console.log(`[server] http鉴权token: ${logger.green(process.env.HTTP_AUTH_KEY)}`)
     console.log(`[server] ws鉴权token: ${logger.green(process.env.WS_SERVER_AUTH_KEY)}`)
   })
 }
+
+/**
+ * web 服务
+ */
+export const web = () => {
+  /** web静态文件目录 */
+  const webDir = path.join(process.cwd(), 'dist/web')
+  const staticFiles = express.static(webDir)
+
+  /** 所有 /web 开头的路由都指向静态文件 */
+  app.use('/web', staticFiles)
+
+  /** 处理 /web 路径下的所有请求，确保 SPA 路由可以正常工作 */
+  app.get('/web/*', (req, res) => {
+    res.sendFile(path.join(webDir, 'index.html'))
+  })
+
+  listeners.once('online', () => {
+    /** 处理根路径请求 */
+    app.get('/', (req, res) => {
+      res.redirect('/web')
+    })
+  })
+}
+
+/** 中间件鉴权 */
+app.use(authMiddleware)
+app.use('/api/v1', router)
+web()
