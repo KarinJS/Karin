333f2e36ea9f146dc42c25436a6ce35116318320
时瑾 <74231782+sj817@users.noreply.github.com>
2025-02-03 12:11:55 +0800

    fix: 聊天列表正确显示头像

--- packages/web/src/hooks/sandbox/use_sandbox_ws.ts
+++ packages/web/src/hooks/sandbox/use_sandbox_ws.ts
@@ -1,3 +1,4 @@
+import { request } from '@/lib/request'
 import type { SandboxBotInfo } from '@/types/sandbox/bot'
 import { useEffect, useState, useRef } from 'react'
 
@@ -14,45 +15,54 @@ function useSandboxWebsoocket () {
   })
 
   useEffect(() => {
-    console.log('WebSocket connecting')
-    if (wsRef.current) return
-    setConnecting(true)
-    const currentURL = new URL(window.location.href)
-    currentURL.protocol = currentURL.protocol.replace('http', 'ws')
-    currentURL.pathname = '/sandbox'
-    const wsURL = currentURL.toString()
-    const ws = new WebSocket(wsURL)
-    wsRef.current = ws
-    ws.onopen = () => {
-      setConnecting(false)
-      setConnected(true)
-      console.log('WebSocket connected')
-    }
-    ws.onmessage = event => {
-      const message = JSON.parse(event.data)
-      console.log(message)
-      if (message.action === 'init') {
-        setReady(true)
-        setBotInfo(message.param)
+    const connectWs = async () => {
+      console.log('WebSocket connecting')
+      if (wsRef.current) return
+      setConnecting(true)
+
+      const { authKey } = await request.serverPost<{ authKey: string }, null>('/api/v1/sandbox/url')
+
+      const currentURL = new URL(window.location.href)
+      currentURL.protocol = currentURL.protocol.replace('http', 'ws')
+      currentURL.pathname = '/sandbox'
+      currentURL.searchParams.set('authorization', authKey)
+      const wsURL = currentURL.toString()
+      const ws = new WebSocket(wsURL)
+      wsRef.current = ws
+      ws.onopen = () => {
+        setConnecting(false)
+        setConnected(true)
+        console.log('WebSocket connected')
       }
-      console.log('WebSocket message received:', event.data)
+      ws.onmessage = event => {
+        const message = JSON.parse(event.data)
+        console.log(message)
+        if (message.action === 'init') {
+          setReady(true)
+          setBotInfo(message.param)
+        }
+        console.log('WebSocket message received:', event.data)
+      }
+      ws.onclose = () => {
+        setConnecting(false)
+        setConnected(false)
+        console.log('WebSocket disconnected')
+      }
+      ws.onerror = error => {
+        setError(error)
+        console.error('WebSocket error:', error)
+      }
     }
-    ws.onclose = () => {
-      setConnecting(false)
-      setConnected(false)
-      console.log('WebSocket disconnected')
-    }
-    ws.onerror = error => {
-      setError(error)
-      console.error('WebSocket error:', error)
-    }
+
+    connectWs()
+
     return () => {
       console.log('WebSocket cleanup')
       setConnecting(false)
       setConnected(false)
       setError(null)
       setReady(false)
-      ws.close()
+      wsRef.current?.close()
       wsRef.current = null
     }
   }, [])
