42c421d295c6666c14f75e2f4a2113fdd1f66fd7
时瑾 <74231782+sj817@users.noreply.github.com>
2025-02-16 10:30:48 +0800

    fix: console adapter基本接口

--- packages/core/src/adapter/input/index.ts
+++ packages/core/src/adapter/input/index.ts
@@ -173,6 +173,101 @@ class AdapterConsole extends AdapterBase implements AdapterType {
   async getGroupAvatarUrl (groupId: string, size?: 0 | 40 | 100 | 140, history?: number) {
     return 'https://p.qlogo.cn/gh/967068507/967068507/0'
   }
+
+  async recallMsg (contact: Contact, messageId: string) {
+    logger.info(`[recallMsg] ${messageId}`)
+    return true
+  }
+
+  async sendLike (targetId: string, count: number) {
+    logger.info(`[sendLike] ${targetId} ${count}`)
+    return true
+  }
+
+  async groupKickMember (groupId: string, targetId: string, rejectAddRequest?: boolean, kickReason?: string) {
+    logger.info(`[groupKickMember] ${groupId} ${targetId} ${rejectAddRequest} ${kickReason}`)
+    return true
+  }
+
+  async setGroupMute (groupId: string, targetId: string, duration: number) {
+    logger.info(`[setGroupMute] ${groupId} ${targetId} ${duration}`)
+    return true
+  }
+
+  async setGroupAllMute (groupId: string, isBan: boolean) {
+    logger.info(`[setGroupAllMute] ${groupId} ${isBan}`)
+    return true
+  }
+
+  async setGroupAdmin (groupId: string, targetId: string, isAdmin: boolean) {
+    logger.info(`[setGroupAdmin] ${groupId} ${targetId} ${isAdmin}`)
+    return true
+  }
+
+  async setGroupMemberCard (groupId: string, targetId: string, card: string) {
+    logger.info(`[setGroupMemberCard] ${groupId} ${targetId} ${card}`)
+    return true
+  }
+
+  async setGroupName (groupId: string, groupName: string) {
+    logger.info(`[setGroupName] ${groupId} ${groupName}`)
+    return true
+  }
+
+  async setGroupQuit (groupId: string, isDismiss: boolean) {
+    logger.info(`[setGroupQuit] ${groupId} ${isDismiss}`)
+    return true
+  }
+
+  async setGroupMemberTitle (groupId: string, targetId: string, title: string) {
+    logger.info(`[setGroupMemberTitle] ${groupId} ${targetId} ${title}`)
+    return true
+  }
+
+  async getFriendList (refresh?: boolean) {
+    logger.info(`[getFriendList] ${refresh}`)
+    return []
+  }
+
+  async getGroupList (refresh?: boolean) {
+    logger.info(`[getGroupList] ${refresh}`)
+    return []
+  }
+
+  async getGroupMemberList (groupId: string, refresh?: boolean) {
+    logger.info(`[getGroupMemberList] ${groupId} ${refresh}`)
+    return []
+  }
+
+  async getGroupHonor (groupId: string) {
+    logger.info(`[getGroupHonor] ${groupId}`)
+    return []
+  }
+
+  async setFriendApplyResult (requestId: string, isApprove: boolean, remark?: string) {
+    logger.info(`[setFriendApplyResult] ${requestId} ${isApprove} ${remark}`)
+    return true
+  }
+
+  async setGroupApplyResult (requestId: string, isApprove: boolean, denyReason?: string) {
+    logger.info(`[setGroupApplyResult] ${requestId} ${isApprove} ${denyReason}`)
+    return true
+  }
+
+  async setInvitedJoinGroupResult (requestId: string, isApprove: boolean) {
+    logger.info(`[setInvitedJoinGroupResult] ${requestId} ${isApprove}`)
+    return true
+  }
+
+  async setMsgReaction (contact: Contact, messageId: string, faceId: number, isSet: boolean) {
+    logger.info(`[setMsgReaction] ${contact} ${messageId} ${faceId} ${isSet}`)
+    return true
+  }
+
+  async uploadFile (contact: Contact, file: string, name: string, folder?: string) {
+    logger.info(`[uploadFile] ${contact} ${file} ${name} ${folder}`)
+    return true
+  }
 }
 
 const adapter = new AdapterConsole()
