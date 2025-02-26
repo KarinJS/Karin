b2a0a4d755ba80df679a939c361f03ed2d25eecc
炫炫 <2066855608@qq.com>
2025-02-26 18:36:29 +0800

    fix: 登录错误只返回失败响应

--- packages/core/src/server/api/utils/get.ts
+++ packages/core/src/server/api/utils/get.ts
@@ -3,14 +3,5 @@ import { router } from '../router'
 import { getAllBotList } from '@/service'
 
 router.get('/utils/get/bots', (req, res) => {
-  const authKey = req.headers.authorization
-  if (authKey && authKey.startsWith('Bearer ')) {
-    const token = authKey.split(' ')[1]
-    if (token === process.env.HTTP_AUTH_KEY) {
-      const bots = getAllBotList()
-      createSuccessResponse(res, bots)
-    } else {
-      createSuccessResponse(res, [])
-    }
-  }
+  createSuccessResponse(res, getAllBotList())
 })
