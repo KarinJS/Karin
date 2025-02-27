813cdfebf4b75515e9f0e1475c2656d10ed78277
炫炫 <2066855608@qq.com>
2025-02-27 08:03:49 +0800

    fix: 404

--- packages/web/src/pages/404.tsx
+++ packages/web/src/pages/404.tsx
@@ -1,14 +1,10 @@
+import PageUnderConstruction from '@/components/page_under_construction'
 import React from 'react'
-import ASCIIText from '@/components/ASCIIText'
 
 const NotFoundPage: React.FC = () => {
   return (
     <div className='min-h-screen flex items-center justify-center'>
-      <ASCIIText
-        text='404'
-        enableWaves
-        asciiFontSize={8}
-      />
+      <PageUnderConstruction />
     </div>
 
   )
