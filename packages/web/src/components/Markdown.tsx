f2a7a376d391da55bc41cb36b62f55423953045a
炫炫 <2066855608@qq.com>
2025-03-08 16:05:16 +0800

    style: a标签文本用主题色加粗

--- packages/web/src/components/Markdown.tsx
+++ packages/web/src/components/Markdown.tsx
@@ -5,7 +5,7 @@ import 'highlight.js/styles/atom-one-dark.css'
 
 const Markdown = ({ content }: { content: string }) => {
   return (
-    <div className='prose prose-zinc max-w-none dark:prose-invert'>
+    <div className='prose prose-zinc prose-a:text-primary prose-a:font-bold  max-w-none dark:prose-invert'>
       <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
         {content}
       </ReactMarkdown>
