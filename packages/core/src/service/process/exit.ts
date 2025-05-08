e971438892fe5726466ddfc1fd408599d8af6457
时瑾 <shijinn520@gmail.com>
2025-05-09 03:29:54 +0800

    fix: 修复主进程退出时子进程未被正确终止的问题 修复场景唯一标识符缓存问题 (#413)

--- packages/core/src/service/process/exit.ts
+++ packages/core/src/service/process/exit.ts
@@ -9,21 +9,24 @@ let exitStatus = false
  * @param code 退出码
  */
 export const processExit = async (code: unknown) => {
+  logger.debug('[child] 子进程收到退出申请:', code)
   try {
     if (exitStatus) return
     exitStatus = true
 
+    logger.debug('[child] 开始保存redis数据')
     const { redis } = await import('@/core/db/redis/redis')
-    await redis.save()
+    if (redis.id === 'mock') await redis.save()
+    logger.debug('[child] redis数据保存完成')
 
     logger.mark(tips(`运行结束 运行时间：${uptime()} 退出码：${code ?? '未知'}`))
     /** 如果是pm2环境 */
     if (process.env.pm_id) {
-      logger.mark(tips('pm2环境 删除pm2进程'))
+      logger.mark(tips('[child] pm2环境 删除pm2进程'))
       await exec(`pm2 delete ${process.env.pm_id}`)
     }
   } catch (error) {
-    logger.error(new Error('退出事件发生错误:', { cause: error }))
+    logger.error(new Error('[child] 退出事件发生错误:', { cause: error }))
   } finally {
     /** exit还会再触发一次事件  */
     setTimeout(() => {
