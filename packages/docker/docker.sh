1bd1bf40fa20ec16e483ee8742bdd12bffa303e0
时瑾 <74231782+sj817@users.noreply.github.com>
2025-05-11 05:31:34 +0800

    ci: 优化docker

--- packages/docker/docker.sh
+++ packages/docker/docker.sh
@@ -52,7 +52,7 @@ install_karin(){
     -e TZ=Asia/Shanghai \
     -e PORT="$PORT" \
     -p $PORT:$PORT \
-    -v /opt/karin:/app \
+    -v /opt/karin:/app/karin \
     $DOCKER_IMAGE
     if ! grep -q "alias karin=" ~/.bashrc; then
         echo "alias karin='docker exec -it karin karin'" >> ~/.bashrc
