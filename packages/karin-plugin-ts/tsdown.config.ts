3da44b3ab958f95fa2519a105db531cb3b1aa1c1
炫炫 <112480306+ikenxuan@users.noreply.github.com>
2026-08-06 13:24:37 +0800

    feat: 更新构建配置，迁移至 tsdown，调整输出目录至 dist (#672)

--- packages/karin-plugin-ts/tsdown.config.ts
+++ packages/karin-plugin-ts/tsdown.config.ts
@@ -1,24 +1,31 @@
-import { defineConfig } from 'tsup'
-import type { Options } from 'tsup'
+import { defineConfig } from 'tsdown'
+import type { UserConfig } from 'tsdown'
 
 /**
- * @description `tsup` configuration options
+ * @description `tsdown` configuration options
  */
-export const options: Options = {
+export const options: UserConfig = {
   entry: ['src/*.ts', 'src/apps/*.ts'], // 入口文件
   format: ['esm'], // 输出格式
   target: 'node18', // 目标环境
-  splitting: true, // 是否拆分文件 启用哦 不然会强制打包的~
   sourcemap: false, // 是否生成 sourcemap
   clean: true, // 是否清理输出目录
   dts: false, // 是否生成 .d.ts 文件 没啥事可以不需要生成类型，除非你的插件会被其他插件调用。
-  outDir: 'lib', // 输出目录
+  outDir: 'dist', // 输出目录
   treeshake: false, // 树摇优化
   minify: false, // 压缩代码
-  external: [
-    'node-karin',
-  ],
+  deps: {
+    neverBundle: [
+      'node-karin',
+      /^node-karin\//,
+    ],
+  },
   shims: true,
+  outExtensions () {
+    return {
+      js: '.js',
+    }
+  },
 }
 
 export default defineConfig(options)
